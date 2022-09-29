1e1a695ecdac39cbc73fb5642c11d65acd5eb53f
Lorene Carniel <lorene.carniel@gmail.com>
2022-09-29 15:20:22 -0300

    Configurações da API

--- src/components/VotingForm.js
+++ src/components/VotingForm.js
@@ -3,33 +3,90 @@ import {
     Button,
     Flex,
     Heading,
-    HStack,
-    PinInput,
-    PinInputField,
+    Input,
+    FormControl,
+    FormLabel,
+    NumberInput,
+    NumberInputField,
+    NumberInputStepper,
+    NumberIncrementStepper,
+    NumberDecrementStepper,
 } from '@chakra-ui/react';
 
 export function VotingForm({ onSubmit }) {
     const [vote, setVote] = useState('');
+    const [city, setCity] = useState('');
+    const [state, setState] = useState('');
+    const [age, setAge] = useState('');
 
-    const _onSubmit = e => {
+    const _onSubmit = async e => {
         e.preventDefault();
-        onSubmit(vote);
+        await onSubmit({
+            vote,
+            city,
+            state,
+            age,
+        });
         setVote('');
+        setCity('');
+        setState('');
+        setAge('');
     };
 
     return (
         <Flex as="form" direction="column" gap="3" onSubmit={_onSubmit}>
-            <Heading>Vote</Heading>
+            <Heading>Votação</Heading>
 
-            <HStack>
-                <PinInput value={vote} onChange={value => setVote(value)}>
-                    <PinInputField />
-                    <PinInputField />
-                </PinInput>
-            </HStack>
+            <FormControl>
+                <FormLabel>Idade</FormLabel>
+                <NumberInput
+                    max={100}
+                    min={18}
+                    type="number"
+                    placeholder="Ex: 18"
+                    value={age}
+                    onChange={value => setAge(value)}
+                >
+                    <NumberInputField />
+                    <NumberInputStepper>
+                        <NumberIncrementStepper />
+                        <NumberDecrementStepper />
+                    </NumberInputStepper>
+                </NumberInput>
+            </FormControl>
+
+            <FormControl>
+                <FormLabel>Cidade</FormLabel>
+                <Input
+                    type="text"
+                    placeholder="Ex: Ribeirão Preto"
+                    value={city}
+                    onChange={e => setCity(e.target.value)}
+                />
+            </FormControl>
+
+            <FormControl>
+                <FormLabel>Estado</FormLabel>
+                <Input
+                    type="text"
+                    placeholder="Ex: São Paulo"
+                    value={state}
+                    onChange={e => setState(e.target.value)}
+                />
+            </FormControl>
+
+            <FormControl>
+                <FormLabel>Voto</FormLabel>
+                <Input
+                    type="number"
+                    placeholder="Ex: 18"
+                    value={vote}
+                    onChange={e => setVote(e.target.value)}
+                />
+            </FormControl>
 
             <Button type="submit" colorScheme="blue" mt="5">
-                Vote
+                Votar
             </Button>
         </Flex>
     );
